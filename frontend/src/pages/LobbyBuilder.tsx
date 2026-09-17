a7b038e886a72e5de396c4a46be93a78393ac5d4
lucrobb <ljrobbiani@icloud.com>
2026-09-17 20:54:10 +0200

    Fixed lobby unused states

--- frontend/src/pages/LobbyBuilder.tsx
+++ frontend/src/pages/LobbyBuilder.tsx
@@ -8,20 +8,13 @@ import { ApiError } from '../api/helper';
 export default function LobbyBuilder() {
     const navigate = useNavigate();
 
-    const [obstacles, setObstacles] = useState<Obstacle[]>([]);
-    const [spawnPosition, setSpawnPosition] = useState<Vector3 | null>(null)
-    const [loading, setLoading] = useState<boolean>(false);
-
     async function handleBuild(): Promise<void> {
-        setLoading(true);
         try {
             const data: Lobby = await createLobby();
             const roomCode = data.roomCode;
             navigate(`/lobby/${roomCode}/`);
         } catch (err) {
             toast.error(err instanceof ApiError ? err.message : "Errore di rete");
-        } finally {
-            setLoading(false);
         }
     }
 
