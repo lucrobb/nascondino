bae8b8b370a38a87021f9ad9f8e6336871634b55
lucrobb <ljrobbiani@icloud.com>
2026-09-18 19:11:07 +0200

    Adjusting id debugging

--- frontend/src/pages/GameRoom.tsx
+++ frontend/src/pages/GameRoom.tsx
@@ -74,8 +74,11 @@ export default function GameRoom() {
     const [isFound, setIsFound] = useState<boolean>(false);
     const [isCreator, setIsCreator] = useState<boolean>(false);
     const playerId = useRef<string>(localStorage.getItem("playerId") ?? crypto.randomUUID());
-    useEffect(() => localStorage.setItem("playerId", playerId.current), []);
-    console.log("Player id:", playerId.current);
+    useEffect(() => {
+        localStorage.setItem("playerId", playerId.current);
+        console.log("Player id:", playerId.current);
+    }, []);
+    
 
     const [players, setPlayers] = useState<Record<string, Player>>({});
     const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
