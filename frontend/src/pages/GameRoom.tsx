672bf8ab78206857b8347cff1175ef4c9af1930b
lucrobb <ljrobbiani@icloud.com>
2026-09-18 19:07:04 +0200

    Added more debugging

--- frontend/src/pages/GameRoom.tsx
+++ frontend/src/pages/GameRoom.tsx
@@ -75,6 +75,7 @@ export default function GameRoom() {
     const [isCreator, setIsCreator] = useState<boolean>(false);
     const playerId = useRef<string>(localStorage.getItem("playerId") ?? crypto.randomUUID());
     useEffect(() => localStorage.setItem("playerId", playerId.current), []);
+    console.log("Player id:", playerId);
 
     const [players, setPlayers] = useState<Record<string, Player>>({});
     const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
